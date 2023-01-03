182f1e8075a52c35b92841e45fd26a7ea5693513
tzz-MBA <tuzhenzhao@aol.com>
2023-01-03 17:18:20 -0600

    v0.3.0 update

---
+++ tranlate_deepl.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TranlateDeepl < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "331380525ac1ba926f0522723e397b17216185a398d8a9631ffbe403bf5114e9"
+  license "MIT"
+
+  depends_on "jq"
+
+  def install
+    bin.install 'trans'
+    prefix.install "README.md"
+    prefix.install "LICENSE"
+  end
+end
