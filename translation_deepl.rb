e2077b7983a7bd6a9cf8d03a75ddcee4689d3e51
tzz-MBA <tuzhenzhao@aol.com>
2023-01-03 17:05:49 -0600

    new rb

--- translation_deepl.rb
+++ translation_deepl.rb
@@ -4,8 +4,8 @@
 class TranslationDeepl < Formula
   desc "this is simple script for translation running by zsh"
   homepage ""
-  url "https://github.com/Tu-Zhenzhao/translation_deepl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f543e42b4bfe1e543640f1f849ff340a0b9083ab5d8554f306ff8da437233f14"
+  url "https://github.com/Tu-Zhenzhao/translation_deepl/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "db01a716d63b62479384bab46aaac215fb6c75b3df9d8f58f588ce700070e409"
   license "MIT"
 
   depends_on "jq"
