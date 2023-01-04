5dd18b0b7fa628d86c456852f2aaafa6e63f8f30
tzz-MBA <tuzhenzhao@aol.com>
2023-01-03 18:03:46 -0600

    add webpage

--- tranlate_deepl.rb
+++ tranlate_deepl.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class TranlateDeepl < Formula
   desc ""
-  homepage ""
+  homepage "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl.git"
   url "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "331380525ac1ba926f0522723e397b17216185a398d8a9631ffbe403bf5114e9"
   license "MIT"
