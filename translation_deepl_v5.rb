d1581e5e1089b9243ef182ab2c8c94521dd7c0b3
tzz-MBA <tuzhenzhao@aol.com>
2023-01-03 18:48:58 -0600

    v0.5.0

--- translation_deepl_v5.rb
+++ translation_deepl_v5.rb
@@ -1,21 +1,22 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class TranslationDeepl < Formula
+class TranslationDeeplV5 < Formula
   desc ""
   homepage "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl"
-  url "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "8ac3ab8156d9a355ec76b216884f3c14bfde810186215b942a4af3963f1f6b66"
+  url "https://github.com/Tu-Zhenzhao/homebrew-translation_deepl/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "4f2e7dcdd899f5100d66e31c08be77869e9e53005cf1b7397b85c213565048d7"
   license "MIT"
 
   depends_on "jq"
- # depends_on "pbcopy"
+# depends_on "pbcopy"
 
   def install
     bin.install 'trans'
     prefix.install "README.md"
     prefix.install "LICENSE"
   end
+
 
 
 
